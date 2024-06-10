5f48fd2b0ded321dc7fb7e0903e069c3992402bc
Lucas Franco <lucasframoon@gmail.com>
2024-06-10 12:13:15 -0300

    HEY-3: refactor UI to use blade components

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,20 @@
+@props([
+    'action',
+    'post' => null,
+    'put' => null,
+    'delete' => null,
+])
+
+<form action="{{$action}}" method="post">
+    @csrf
+
+    @if ($put)
+        @method('PUT')
+    @endif
+
+    @if ($delete)
+        @method('DELETE')
+    @endif
+
+    {{$slot}}    
+</form>
