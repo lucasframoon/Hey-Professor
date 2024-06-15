f58bd81c4c7fabfccf103f5086ca1dc7dff81fb4
Lucas Franco <lucasframoon@gmail.com>
2024-06-15 18:03:52 -0300

    HEY-11: Add dockblock

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -48,21 +49,41 @@ class User extends Authenticatable implements MustVerifyEmail
         ];
     }
 
+    /**
+    * Retrieve the votes associated with this user.
+    *
+    * @return HasMany<Vote>
+    */
+    public function votes(): HasMany
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+    /**
+    * Create a vote for the user to like a question.
+    *
+    * @param Question $question The question to like.
+    * @return void
+    */
     public function like(Question $question): void
     {
-        Vote::query()->create([
+        $this->votes()->create([
             'question_id' => $question->id,
-            'user_id'     => $this->id,
             'like'        => 1,
             'unlike'      => 0,
         ]);
     }
 
+    /**
+    * Create a vote for the user to unlike a question.
+    *
+    * @param Question $question The question to unlike.
+    * @return void
+    */
     public function unlike(Question $question): void
     {
-        Vote::query()->create([
+        $this->votes()->create([
             'question_id' => $question->id,
-            'user_id'     => $this->id,
             'like'        => 0,
             'unlike'      => 1,
         ]);
